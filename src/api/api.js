6e41ff825eaea9f6ed0eacef3f15cd405a7de2b4
Carlos Jr <jc.carlos.junior@outlook.com>
2025-12-22 11:02:48 -0300

    base url for api changed

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from '@/router'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1/',
+  baseURL: 'https://expertconsig.pythonanywhere.com/admin/api/v1/',
   headers: {
     'Content-Type': 'application/json'
   }
